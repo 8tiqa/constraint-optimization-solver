b0e5a657ef8a3f768c01b7229ac52af0c2438504
Atiqa Zafar <12beseazafar@seecs.edu.pk>
2016-05-16 00:35:48 +0500

    Add files via upload

---
+++ Examples/production.sol
@@ -0,0 +1,12 @@
+# Objective value = 70500000
+Regular_1 7000
+Regular_2 10000
+Regular_3 10000
+Regular_4 8000
+Overtime_1 0
+Overtime_2 0
+Overtime_3 0
+Overtime_4 0
+Inventory_1 5000
+Inventory_2 0
+Inventory_3 0
